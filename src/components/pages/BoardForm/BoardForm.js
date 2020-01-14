47d126dbcd551206049869590121b02ce9f70cf9
John Thielman <john.thielman@gmail.com>
2020-01-13 21:05:44 -0600

    update board works

--- src/components/pages/BoardForm/BoardForm.js
+++ src/components/pages/BoardForm/BoardForm.js
@@ -32,6 +32,19 @@ class BoardForm extends React.Component {
     this.setState({ boardDescription: e.target.value });
   }
 
+  editBoardEvent = (e) => {
+    e.preventDefault();
+    const { boardId } = this.props.match.params;
+    const editBoard = {
+      name: this.state.boardName,
+      description: this.state.boardDescription,
+      uid: authData.getUid(),
+    };
+    boardData.updateBoard(boardId, editBoard)
+      .then(() => this.props.history.push('/'))
+      .catch((err) => console.error('error from edit board', err));
+  }
+
   saveBoardEvent = (e) => {
     e.preventDefault();
     const newBoard = {
@@ -46,6 +59,7 @@ class BoardForm extends React.Component {
 
   render() {
     const { boardName, boardDescription } = this.state;
+    const { boardId } = this.props.match.params;
     return (
       <form className="BoardForm container">
         <div className='form-group'>
@@ -70,7 +84,10 @@ class BoardForm extends React.Component {
             onChange={this.descriptionChange}
           />
         </div>
-        <button className='btn btn-outline-primary' onClick={this.saveBoardEvent}>Save Board</button>
+        { boardId
+          ? <button className='btn btn-outline-primary' onClick={this.editBoardEvent}>Edit Board</button>
+          : <button className='btn btn-outline-primary' onClick={this.saveBoardEvent}>Save Board</button>
+        }
       </form>
     );
   }
